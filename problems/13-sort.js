e4dc117d97d4b3816bf4c6a0dba7d02ee93c5f0c
Dreklorian <sarkos381@yahoo.com>
2023-11-23 12:27:00 -0800

    2 more

--- problems/13-sort.js
+++ problems/13-sort.js
@@ -22,9 +22,22 @@ sort([]); // []
 ***********************************************************************/
 
 function sort(nums, sorted = []) {
-  // your code here
+  if (nums.length === 0) return sorted;
+  let smallest = Infinity;
+  let index = 0;
+  nums.forEach((n, i) => {
+    if (n < smallest) {
+      smallest = n;
+      index = i;
+    }
+  })
+  sorted.push(smallest);
+  nums.splice(index, 1);
+  return sort(nums, sorted);
 }
-
+console.log(sort([4,1,6,3,1,7])); // [1, 1, 3, 4, 6, 7]
+console.log(sort([0, 1, -3])); // [-3, 0, 1]
+console.log(sort([])); // []
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = sort;
