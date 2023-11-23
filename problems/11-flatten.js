49cdd3a9151c7c92b9d5a00573550b267eae8aa6
Dreklorian <sarkos381@yahoo.com>
2023-11-23 08:21:46 -0800

    3 more

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -10,8 +10,29 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
-// your code here
-  
+function flatten(arr) {
+  let newArr = [];
+  arr.forEach((item) => {
+    if (Array.isArray(item)) {
+      newArr.push(...flatten(item));
+    } else {
+      newArr.push(item);
+    }
+  })
+  return newArr;
+  // if (arr.length === 0) return newArr;
+  // if (!Array.isArray(arr[0])) {
+  //   newArr.push(arr[0]);
+  //   arr.shift()
+  //   return flatten(arr);
+  // } else {
+  //   return flatten(arr[0]);
+  // }
+
+}
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3, 4], 6]])); // [1, 2, 3]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = flatten;
--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -10,8 +10,19 @@ isSorted([2, 4, 6, 7, 8]); // true
 isSorted([5, 4, 3, 2, 1]); // false
 ***********************************************************************/
 
-// your code here
-
+function isSorted(arr) {
+  if (arr.length === 1) return true;
+  if (arr[0] < arr[1]) {
+    arr.shift();
+    return isSorted(arr);
+  } else {
+    return false;
+  }
+}
+console.log(isSorted([1, 2, 3, 4, 5])); // true
+console.log(isSorted([1, 2, 4, 3, 5])); // false
+console.log(isSorted([2, 4, 6, 7, 8])); // true
+console.log(isSorted([5, 4, 3, 2, 1])); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = isSorted;
