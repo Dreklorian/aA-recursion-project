eb765ff3417d92f448055d281b95a363e139f6a8
Dreklorian <sarkos381@yahoo.com>
2023-11-22 16:12:29 -0800

    2 more

--- problems/08-advanced-exponent.js
+++ problems/08-advanced-exponent.js
@@ -41,8 +41,38 @@ times `advancedExponent` is being recursively called.
 
 
 function advancedExponent(b, n) {
-  // your code here
+  // if (n <= 1) {
+  //   return 1;
+  // } else if (n % 2 === 0) {
+  //   let n2 = n/2;
+  //   let b2 = advancedExponent(b, n2)
+  //   return b2 * b2  ** 2;
+  // // } else {
+  // //   return b * advancedExponent(b, n - 1);
+  // }
+
+  //console.log("Exponent(" + b + ", " + n + ")");
+  if (n === 0) {
+    return 1;
+  } else if (n % 2 === 0) {
+    return advancedExponent(b * b, n / 2);
+  } else {
+    return b * advancedExponent(b * b, (n - 1) / 2);
+  }
 }
+console.log(advancedExponent(2, 0)); // 1
+console.log(advancedExponent(2, 1)); // 2
+console.log(advancedExponent(2, 2)); // 4
+console.log(advancedExponent(2, 3)); // 8
+console.log(advancedExponent(2, 4)); // 16
+console.log(advancedExponent(2, 5)); // 32
+console.log(advancedExponent(2, 6)); // 64
+console.log(advancedExponent(2, 7)); // 128
+console.log(advancedExponent(2, 8)); // 256
+console.log(advancedExponent(2, 9)); // 512
+console.log(advancedExponent(2, 10)); // 1024
+console.log(advancedExponent(2, 11)); // 2048
+console.log(advancedExponent(5, 12)); // 4096
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
